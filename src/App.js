77730c657ac8a8fcec1b44f5c8ecfe3f3e251d18
Irvyn Cornejo <irvyncornejo@gmail.com>
2022-10-19 00:08:47 -0500

    add rxjs and router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import ProfessionalPortfolio  from './components/ProfessionalPortfolio'
-
+import Projects from './components/Projects/Projects';
 const App = () => {
   return (
     <>
-      <div className="App">
-        <ProfessionalPortfolio />
-      </div>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ProfessionalPortfolio />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='*' element={<>NOT FOUND</>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
     </> 
   )
 }
--- src/components/ProfessionalPortfolio/ProfessionalPortfolio.jsx
+++ src/components/ProfessionalPortfolio/ProfessionalPortfolio.jsx
@@ -9,24 +9,28 @@ import IconMenu from "../Navbar/LateralMenu"
 import Footer from "../Footer"
 
 import '../../App.css'
+import { sharingInformationService } from "../../services/shared-services/shared-information.services"
 
 const ProfessionalPortFolio = () => {
   const [ stateTheme, setStateTheme ] = useState(false)
-  const theme =  stateTheme ? <FaMoon />  : <FaSun />
-
+  const theme =  stateTheme ?  <FaSun /> : <FaMoon />
+  const handleTheme = () => {
+    const color =  stateTheme ? '#f50057' : '#00c5b9'
+    sharingInformationService.setSubject(color)
+  }
   const matches = useMediaQuery('(min-width:650px)')
 
     return (
       <>
         <Box sx={{ flexGrow: 1 }}>
           <Grid container spacing={2}>
-            <Grid item xs={2} md={2} lg={3} align="center" className="center-column">
+            <Grid item xs={1} md={2} lg={3} align="center" className="center-column">
             { matches ? <IconMenu /> : '' }
             </Grid>
-            <Grid item xs={8} md={8} lg={6} align="center" className="center-column">
+            <Grid item xs={10} md={8} lg={6} align="center" className="center-column">
               <ProfesionalInformation />
             </Grid>
-            <Grid item xs={2} md={2} lg={3} className="center-column button-theme">
+            <Grid item xs={1} md={2} lg={3} className="center-column button-theme">
               <Button 
                   variant="outlined" 
                   startIcon={ theme }
@@ -38,7 +42,11 @@ const ProfessionalPortFolio = () => {
                       textTransform: 'none'
                     }
                   }
-                  onClick={() => setStateTheme(!stateTheme) }
+                  onClick={ () => {
+                      setStateTheme(!stateTheme)
+                      handleTheme()
+                    }
+                  }
               >Theme
               </Button>
             </Grid>
